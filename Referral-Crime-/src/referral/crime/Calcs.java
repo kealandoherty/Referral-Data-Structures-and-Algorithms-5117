8d3bd574fcaac3456a954397bb7db6b8c40022e8
kealandoherty <40470263+kealandoherty@users.noreply.github.com>
2018-07-17 15:10:59 +0100

    first csv file read
    first csv file is now reading and displaying whenver i run the scancsv file by itself just to get the other two files to read and get the calculations done

--- Referral-Crime-/src/referral/crime/Calcs.java
+++ Referral-Crime-/src/referral/crime/Calcs.java
@@ -5,29 +5,29 @@ import java.util.ArrayList;
 import java.text.DecimalFormat;
 
 public class Calcs {
-    DecimalFormat DF = new DecimalFormat(", ");
-    static ArrayList < HashMap < String, String >> newData = new ArrayList < HashMap < String, String >> ();
- 
-    public static void getCrimeType(String value1) {
-        for (HashMap hm: ScanCSV.CrimeData) {
-            if ((hm.get("Crime Type").equals(value1))) {
-                
-                newData.add(hm);
-            
-            
-            }
-            
-        }
-         newData.forEach(System.out::println);
-     
-    }
-    
-    
-    public static void crimedetails (String key2, String value2) {
-        
-    }
-    
-    
+//    DecimalFormat DF = new DecimalFormat(", ");
+//    static ArrayList < HashMap < String, String >> newData = new ArrayList < HashMap < String, String >> ();
+// 
+//    public static void getCrimeType(String value1) {
+//        for (HashMap hm: ScanCSV.CrimeData) {
+//            if ((hm.get("Crime Type").equals(value1))) {
+//                
+//                newData.add(hm);
+//            
+//            
+//            }
+//            
+//        }
+//         newData.forEach(System.out::println);
+//     
+//    }
+//    
+//    
+//    public static void crimedetails (String key2, String value2) {
+//        
+//    }
+//    
+//    
     
 }
     
--- Referral-Crime-/src/referral/crime/ReferralCrime.java
+++ Referral-Crime-/src/referral/crime/ReferralCrime.java
@@ -3,19 +3,9 @@ package referral.crime;
  import java.io.*;
  import java.util.Scanner;
 
-
 public class ReferralCrime {
-    static Scanner S = new Scanner(System.in);
-   
     public static void main(String[] args) throws IOException{
             
-          //  ScanData.scanAllData();
-            
-            
-            int choice = 0 ; 
-            while (choice !=6) {
-        
-        
          System.out.println("--   Menu   --");
             System.out.println(" ");
             System.out.println("1 - Crime Types ");
@@ -26,40 +16,41 @@ public class ReferralCrime {
             System.out.println("6 - Highest Crime frequency for a user-specified crime type ");
             System.out.println(" ");
             System.out.println("Select Number : ");
-            choice = S.nextInt();
+             Scanner S = new Scanner(System.in);
+             int choice = S.nextInt();
            
-              switch (choice) {
+               switch (choice) {
                 case 1:
                     {
-                        Calcs.getCrimeType(Crimetype());
+                      CrimeType();
                                 }
                 break;
 			
 		case 2 : 
                         {
-					
-                        }
+			CrimeDetails();		
+                                }
 		break;
 			
 		case 3 : 
                         {
-					
+			CrimeStatus();		
                         }
                 break;
                 
                 case 4 : 
                         {
-		
+                        CrimeFreq();
 			}
                 break;
               
                 case 5 : 
                         {
-				
+			UnresolvedCrime();	
                         }
             break;
                 case 6:   {
-		
+                        CrimeFreqUserSpecified();
 			}
                 break;
             
@@ -67,33 +58,83 @@ public class ReferralCrime {
             
               S.close();
             
-          
-          
-          
-          
-            //crime types 
-           
-            //dkisplay all crimes across all LSOAs
-            
-            // determine how many crimes are under investigation and investigation complete "NS"
-            
-            // LSOA with the highest average total crime frequency 
-            
-            // LSOA with the highest average unresolved crimed frequency 
-            
+    }
+    //crime types 
+          private static void CrimeType()
+          {
+              
+             System.out.println("What Crime are you looking for");
+              
+             Scanner S1 = new Scanner(System.in);
+             String UserCrimeType = S1.nextLine();
+              
+             
+              
+          }   
+        //dkisplay all crimes across all LSOAs     
+          private static void CrimeDetails()
+                  {
+                   System.out.println("Here are the details of all crimes across all LSOAS");
+                      
+                     // String Details
+                      
+                  }
+          // determine how many crimes are under investigation and investigation complete "NS"
+           private static void CrimeStatus()
+                  {
+                  System.out.println("Are you looking for crimes that are underinvestigation or are complete? ");
+                  
+                  Scanner S3 = new Scanner(System.in);
+                  String CStat = S3.nextLine();
+                  
+                  
+                  }
+          // LSOA with the highest average total crime frequency 
+           private static void CrimeFreq()
+           {
+               System.out.println("LSOA with the highest average total crime frequency is:"      );
+               
+               
+           }
+           // LSOA with the highest average unresolved crimed frequency 
+           private static void UnresolvedCrime()
+           {
+                System.out.println("LSOA with the highest average unresolved crime frequency is:"      );
+               
+               
+           }
             // LSOA with the highesst crime frequency for a user-specified crime type 
+           private static void CrimeFreqUserSpecified()
+           {
+                System.out.println("LSOA with the highest crime rate for the crime type specified is: ");
+           }
+
+    
+
+          
+            
+           
+            
+            
+            
+            
+            
+            
+            
+            
+           
  
             
-              }
-            }
-
+              
+            
+}
 //    private static void getCrimeType() {
 //        
 //        
 //        
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 //    }
-    }
+   
 
 
    
